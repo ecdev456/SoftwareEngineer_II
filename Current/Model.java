c829149e99f35815a6d3601fe6cd3049e5ab2bc3
ecdev456 <30885893+ecdev456@users.noreply.github.com>
2018-05-10 16:06:28 -0700

    Add files via upload
    updated missing code for Vertical checking

--- Current/Model.java
+++ Current/Model.java
@@ -4,7 +4,7 @@ package ecdev456;
 		
 public class Model {
 	//All values are active, then turned off when clicked on (following game rules)
-	public static final int actNum[][] =
+	public static int actNum[][] =
 		{
 			{ 1,1,1,1,1 },
 			{ 1,1,1,1,1 },
@@ -12,16 +12,22 @@ public class Model {
 			{ 1,1,1,1,1 },
 			{ 1,1,1,1,1 }	
 		};
-	int rulloGrid[][]; //5x5 official grid
+	public static final int rulloGrid[][]=	//5x5 grid official grid
+		{
+				{ 3,2,6,5,2 },
+				{ 3,7,1,1,7 },
+				{ 5,1,5,2,6 },
+				{ 2,9,6,6,7 },
+				{ 2,6,5,2,4 }
+			};//rullo grid
+	public static int hAct[] = {0,0,0,0,0}; //set to not finished
+	public static int vAct[]= {0,0,0,0,0}; //set to not finished
 	
-	public static final int hAct[] = {0,0,0,0,0}; //set to not finished
-	public static final int vAct[]= {0,0,0,0,0}; //set to not finished
+	public static int hSum[] = {13,16,12,23,02};
+	public static int vSum[] = {9,17,13,12,15};
 	
-	int hSum[] = {13,16,12,23,02};
-	int vSum[] = {9,17,13,12,15};
-	
-	private boolean HCheck[]; //size 5, boolean array where each is assigned true or false if valid mathematics for pertaining row.
-	private boolean VCheck[]; //size 5, boolean array where each is assigned true or false if valid mathematics for pertaining column.
+	private static boolean HCheck[]; //size 5, boolean array where each is assigned true or false if valid mathematics for pertaining row.
+	private static boolean VCheck[]; //size 5, boolean array where each is assigned true or false if valid mathematics for pertaining column.
 	
 	public static int CheckActive(int x, int y) {return actNum[x][y];}//getter
 	public static void SetActive(int x, int y, int act) {actNum[x][y] = act;} //Setting active or Not Active
@@ -36,8 +42,9 @@ public class Model {
 	boolean HPrint(int i) { return HCheck[i];}
 	boolean VPrint(int i) { return VCheck[i];}
 	
-	void CheckMath()
+	public static void CheckMath()
 	{
+		System.out.println("Inside Check Math");
 		//Horizontal math checking.  Seperate 2-d array checking active/non-active values
 		//active values will be added and checked to the Final sum of that Row.
 		//Non-active will not be added.
@@ -55,6 +62,10 @@ public class Model {
 				if(actNum[j][i] == 1)
 				{
 					HSum += rulloGrid[j][i];
+				}
+				if (actNum[i][j] == 1) // check vertically by indexing through columns for ON value
+				{
+					JSum += rulloGrid[i][j];
 				}
 				//checking each spot on row, will later change row when 'j++' reached
 			}
@@ -80,9 +91,18 @@ public class Model {
 			j++;
 			col++;
 		}
-		
+		System.out.println("Horizontal");	
+		for(int i = 0; i < 5;i++)
+		{
+			System.out.println(hAct[i]);
+		}
+		System.out.println("Vertical");
+		for(int i = 0; i < 5;i++)
+		{
+			System.out.println(vAct[i]);
+		}
 	}
-	boolean CheckWin()
+	public static boolean CheckWin()
 	{
 		for (int i = 0; i < 5; i++)
 		{
